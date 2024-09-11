1882db8d24f75225e4a3755ef89494fcc53abfe6
Kanishk-000 <kanishk1184@gmail.com>
2024-09-11 18:14:28 +0530

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
     <>
       <div className="container mx-auto p-1">
         <h1 className='text-center text-5xl text-white'>Todo App</h1>
-        <div className="app flex flex-col justify-center min-h-screen py-5">
+        <div className="app flex flex-col justify-center min-h-screen py-5 w-[85vw]">
           <div className="add flex justify-center px-11 gap-5">
             <input type="text" name="addTodo" onChange={handleChange} className='bg-slate-500 border-gray-50 placeholder:px-3 px-3 rounded-lg text-white placeholder:text-gray-400 w-3/4 focus:outline-none focus:bg-slate-700 transition-all' placeholder='Enter Your Todo' value={todo}/>
             <button className='text-white bg-gray-500 w-20 text-lg h-max rounded-lg py-1 hover:bg-slate-700 transition-all' onClick={handleAdd}>Save</button>
@@ -60,7 +60,7 @@ function App() {
             {todos.map((item)=>{
               return (
                 <div key={item.id} className="todo flex justify-between px-10 my-5 items-center">
-                  <div className="text text-white w-3/4 break-words">{item.text}</div>
+                  <div className="text text-white w-1/2 break-words">{item.text}</div>
                   <div className="buttons flex justify-center items-center gap-5">
                     <button onClick={()=>{handleEdit(item.id)}} className='text-white bg-gray-500 w-12 h-10 text-lg rounded-lg py-1 hover:bg-slate-700 transition-all flex justify-center items-center'><img src={edit} alt="" className='w-6'/></button>
                     <button onClick={()=>{handleDelete(item.id)}} className='text-white bg-gray-500 w-12 h-10 text-lg rounded-lg py-1 hover:bg-slate-700 transition-all flex justify-center items-center'><img src={del} alt="" className='w-8'/></button>
